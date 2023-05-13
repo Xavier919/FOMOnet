c511989c440b051b65a716854c63d463aad85d4f
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-05-13 02:32:47 -0400

    graphs

--- prog/modules/get_graph.py
+++ prog/modules/get_graph.py
@@ -16,7 +16,6 @@ from sklearn import metrics
 
 parser = argparse.ArgumentParser()
 parser.add_argument('reports')
-parser.add_argument('bins')
 parser.add_argument('tag', type=str)
 args = parser.parse_args()
 
@@ -24,79 +23,29 @@ if __name__ == "__main__":
 
     reports = pickle.load(open(args.reports, 'rb'))
 
-    #preds_ = [x['out'] for x in reports.values()]
-    #target_ = [x['mapped_cds'] for x in reports.values()]
+    preds_ = [x['out'] for x in reports.values()]
+    target_ = [x['mapped_cds'] for x in reports.values()]
 
-    #preds = cat(preds_)
-    #target = cat(target_).long()
-    #pr_curve = PrecisionRecallCurve(pos_label=1,task="binary")
-    #precision, recall, _ = pr_curve(preds, target)
-    #auc_pr = auc(recall, precision)
-    #plt.plot(recall, precision, color = 'black')
-    #plt.ylim(0.9, 1.05), plt.xlim(0, 1)
-    #plt.xlabel("Recall"), plt.ylabel("Precision"), plt.title('PR curve')
-    #plt.legend(['PR AUC: {}'.format(round(auc_pr, 3))])
-    #plt.savefig(f'pr_curve{args.tag}.svg')
-    #plt.clf()
+    preds = cat(preds_)
+    target = cat(target_).long()
+    pr_curve = PrecisionRecallCurve(pos_label=1,task="binary")
+    precision, recall, _ = pr_curve(preds, target)
+    auc_pr = auc(recall, precision)
+    plt.plot(recall, precision, color = 'lime')
+    plt.ylim(0.9, 1.05), plt.xlim(0, 1)
+    plt.xlabel("Recall"), plt.ylabel("Precision"), plt.title('PR curve')
+    plt.legend(['PR AUC: {}'.format(round(auc_pr, 3))])
+    plt.savefig(f'pr_curve{args.tag}.svg')
+    plt.clf()
 
-    #preds = cat(preds_).detach().numpy()
-    #target = cat(target_).long().detach().numpy()
-    #fpr, tpr, _ = metrics.roc_curve(target, preds)
-    #roc_auc = auc(fpr, tpr)
-    #plt.plot(fpr, tpr, color = 'black')
-    #plt.ylim(0.9, 1.05), plt.xlim(0, 1)
-    #plt.xlabel("FPR"), plt.ylabel("TPR"), plt.title('ROC curve')
-    #plt.legend(['ROC AUC: {}'.format(round(roc_auc, 3))])
-    #plt.savefig(f'roc_curve{args.tag}.svg')
-    #plt.clf()
-
-
-
-
-    for bin_ in args.bins:
-        preds = [y['out'] for x,y in reports.items() if x in bin_]
-        target = [y['mapped_cds'] for x,y in reports.items() if x in bin_]
-        preds = cat(preds).detach().numpy()
-        target = cat(target).long().detach().numpy()
-        fpr, tpr, _ = metrics.roc_curve(target, preds)
-        roc_auc = auc(fpr, tpr)
-        plt.scatter(fpr, tpr, color = 'grey')
-        
-    preds = [y['out'] for x,y in reports.items()]
-    target = [y['mapped_cds'] for x,y in reports.items()]
-    preds = cat(preds).detach().numpy()
-    target = cat(target).long().detach().numpy()
+    preds = cat(preds_).detach().numpy()
+    target = cat(target_).long().detach().numpy()
     fpr, tpr, _ = metrics.roc_curve(target, preds)
     roc_auc = auc(fpr, tpr)
-    plt.plot(fpr, tpr, color = 'green')
-    
+    plt.plot(fpr, tpr, color = 'lime')
     plt.ylim(0.9, 1.05), plt.xlim(0, 1)
     plt.xlabel("FPR"), plt.ylabel("TPR"), plt.title('ROC curve')
     plt.legend(['ROC AUC: {}'.format(round(roc_auc, 3))])
     plt.savefig(f'roc_curve{args.tag}.svg')
     plt.clf()
 
-
-
-    for bin_ in args.bins:
-        preds = [y['out'] for x,y in reports.items() if x in bin_]
-        target = [y['mapped_cds'] for x,y in reports.items() if x in bin_]
-        preds = cat(preds)
-        target = cat(target).long()
-        pr_curve = PrecisionRecallCurve(pos_label=1,task="binary")
-        precision, recall, _ = pr_curve(preds, target)
-        auc_pr = auc(recall, precision)
-        plt.scatter(fpr, tpr, color = 'grey')
-        
-    preds = cat(preds)
-    target = cat(target).long()
-    pr_curve = PrecisionRecallCurve(pos_label=1,task="binary")
-    precision, recall, _ = pr_curve(preds, target)
-    auc_pr = auc(recall, precision)
-    plt.plot(recall, precision, color = 'green')
-    
-    plt.ylim(0.9, 1.05), plt.xlim(0, 1)
-    plt.xlabel("Recall"), plt.ylabel("Precision"), plt.title('PR curve')
-    plt.legend(['PR AUC: {}'.format(round(auc_pr, 3))])
-    plt.savefig(f'pr_curve{args.tag}.svg')
-    plt.clf()
