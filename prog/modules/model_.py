2d318b0b8b8c8dc8736fde5ff72ef7f2222dde36
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-06-30 13:55:05 -0400

    no m

--- prog/modules/model_.py
+++ prog/modules/model_.py
@@ -5,129 +5,57 @@ from torch.nn import functional as F
 
 class FOMOnet(nn.Module):
 
-    def __init__(self, num_channels=4):
+    def __init__(self, enc_chs=(4,32,64,128,256,512,1024), dec_chs=(1024,512,256,128,64,32)):
         super().__init__()
 
-        self.maxpool = nn.MaxPool1d(kernel_size=2)
+        self.enc_chs = enc_chs
+        self.dec_chs = dec_chs
 
-        self.conv_encoder1 = self.conv_block(num_channels, 32)
-        self.conv_encoder2 = self.conv_block(32, 64)
-        self.conv_encoder3 = self.conv_block(64, 128)
-        self.conv_encoder4 = self.conv_block(128, 256)
-        self.conv_encoder5 = self.conv_block(256, 512)
-        self.conv_encoder6 = self.conv_block(512, 1024)
-        self.bottleneck = self.conv_block(1024, 1024)
+    def encoder(self, x):
+        maxpool = nn.MaxPool1d(kernel_size=2)
+        init_shape = x.shape[2]
+        enc_ftrs = []
+        for i, ch in enumerate(self.enc_chs):
+            if ch != self.enc_chs[-1]:
+                self.conv_block = self.conv_block(self.enc_chs[i], self.enc_chs[i+1])
+                self.res_block = self.res_block(self.enc_chs[i], self.enc_chs[i+1])
+                conv_block, res_block = self.conv_block(x), self.res_block(x)
+                conv_block += res_block
+                enc_ftrs.append(conv_block)
+                x = maxpool(conv_block)
+            else:
+                self.conv_block = self.conv_block(self.enc_chs[i], self.enc_chs[i])
+                self.res_block = self.res_block(self.enc_chs[i], self.enc_chs[i])
+                conv_block, res_block = self.conv_block(x), self.res_block(x)
+                x = conv_block + res_block
+        return x, enc_ftrs, init_shape
 
-        self.res_encoder1 = self.res_block(num_channels, 32)
-        self.res_encoder2 = self.res_block(32, 64)
-        self.res_encoder3 = self.res_block(64, 128)
-        self.res_encoder4 = self.res_block(128, 256)
-        self.res_encoder5 = self.res_block(256, 512)
-        self.res_encoder6 = self.res_block(512, 1024)
-        self.res_bottleneck = self.res_block(1024, 1024)
+    def decoder(self, x, enc_ftrs, init_shape):
+        for i, ch in enumerate(self.dec_chs):
+            if ch != self.dec_chs[-1]:
+                transpose = nn.ConvTranspose1d(in_channels=self.dec_chs[i], out_channels=self.dec_chs[i+1], kernel_size=2, stride=2)
+                transpose_ = transpose(x)
+                cropped = self.crop(transpose_, enc_ftrs[i])
+                cat = torch.cat((transpose_, cropped), 1)
+                self.conv_block = self.conv_block(self.dec_chs[i], self.dec_chs[i+1])
+                self.res_block = self.res_block(self.dec_chs[i], self.dec_chs[i+1])
+                conv_block, res_block = self.conv_block(cat), self.res_block(cat)
+                x = conv_block + res_block
+            else:
+                x = self.final_block(x)
+                out = F.interpolate(x, init_shape)
+        return out
 
-        self.conv_decoder4 = self.conv_block(1024, 512)
-        self.conv_decoder3 = self.conv_block(512, 256)
-        self.conv_decoder2 = self.conv_block(256, 128)
-        self.conv_decoder1 = self.conv_block(128, 64)
-        self.conv_decoder0 = self.conv_block(64, 32)
-        self.final_layer = self.final_block(32, 1)
-
-        self.res_decoder4 = self.res_block(1024, 512)
-        self.res_decoder3 = self.res_block(512, 256)
-        self.res_decoder2 = self.res_block(256, 128)
-        self.res_decoder1 = self.res_block(128, 64)
-        self.res_decoder0 = self.res_block(64, 32)
-
-        self.upsample1 = nn.ConvTranspose1d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
-        self.upsample2 = nn.ConvTranspose1d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
-        self.upsample3 = nn.ConvTranspose1d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
-        self.upsample4 = nn.ConvTranspose1d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
-        self.upsample5 = nn.ConvTranspose1d(in_channels=64, out_channels=32, kernel_size=2, stride=2)
-        
+    def forward(self, x):
+        sigmoid = nn.Sigmoid()
+        x, enc_ftrs, init_shape = self.encoder(x)
+        out = self.decoder(x, enc_ftrs, init_shape)
+        return sigmoid(out)
 
     def crop(self, x, enc_ftrs):
         chs, dims = x.shape[1:]
         enc_ftrs = torchvision.transforms.CenterCrop([chs, dims])(enc_ftrs)
         return enc_ftrs
-
-    def forward(self, x):
-
-        #encoder
-        encode_block1 = self.conv_encoder1(x)
-        residual_block1 = self.res_encoder1(x)
-        encode_block1 += residual_block1
-        encode_block1 = self.maxpool(encode_block1)
-
-        encode_block2 = self.conv_encoder2(encode_block1)
-        residual_block2 = self.res_encoder2(encode_block1)
-        encode_block2 += residual_block2
-        encode_block2 = self.maxpool(encode_block2)
-
-        encode_block3 = self.conv_encoder3(encode_block2)
-        residual_block3 = self.res_encoder3(encode_block2)
-        encode_block3 += residual_block3
-        encode_block3 = self.maxpool(encode_block3)
-
-        encode_block4 = self.conv_encoder4(encode_block3)
-        residual_block4 = self.res_encoder4(encode_block3)
-        encode_block4 += residual_block4
-        encode_block4 = self.maxpool(encode_block4)
-
-        encode_block5 = self.conv_encoder5(encode_block4)
-        residual_block5 = self.res_encoder5(encode_block4)
-        encode_block5 += residual_block5
-        encode_block5 = self.maxpool(encode_block5)
-
-        encode_block6 = self.conv_encoder6(encode_block5)
-        residual_block6 = self.res_encoder6(encode_block5)
-        encode_block6 += residual_block6
-        encode_block6 = self.maxpool(encode_block6)
-
-        #bottleneck
-        bottleneck = self.bottleneck(encode_block6)
-        residual_bot = self.res_bottleneck(encode_block6)
-        bottleneck += residual_bot
-
-        #middle
-        decode_middle = self.upsample1(bottleneck)
-
-        #decoder
-        cropped5 = self.crop(decode_middle, encode_block5)
-        decode_block5 = torch.cat((decode_middle, cropped5), 1)
-        cat_layer4 = self.conv_decoder4(decode_block5)
-        cat_layer4 += self.res_decoder4(decode_block5)
-        cat_layer4 = self.upsample2(cat_layer4)
-
-        cropped4 = self.crop(cat_layer4, encode_block4)
-        decode_block4 = torch.cat((cat_layer4, cropped4), 1)
-        cat_layer3 = self.conv_decoder3(decode_block4)
-        cat_layer3 += self.res_decoder3(decode_block4)
-        cat_layer3 = self.upsample3(cat_layer3)
-
-        cropped3 = self.crop(cat_layer3, encode_block3)
-        decode_block3 = torch.cat((cat_layer3, cropped3), 1)
-        cat_layer2 = self.conv_decoder2(decode_block3)
-        cat_layer2 += self.res_decoder2(decode_block3)
-        cat_layer2 = self.upsample4(cat_layer2)
-
-        cropped2 = self.crop(cat_layer2, encode_block2)
-        decode_block2 = torch.cat((cat_layer2, cropped2), 1)
-        cat_layer1 = self.conv_decoder1(decode_block2)
-        cat_layer1 += self.res_decoder1(decode_block2)
-        cat_layer1 = self.upsample5(cat_layer1)
-
-        cropped1 = self.crop(cat_layer1, encode_block1)
-        decode_block1 = torch.cat((cat_layer1, cropped1), 1)
-        cat_layer0 = self.conv_decoder0(decode_block1)
-        cat_layer0 += self.res_decoder0(decode_block1)
-
-        final_layer = self.final_layer(cat_layer0)
-
-        out = F.interpolate(final_layer, x.shape[2])
-        sigmoid = nn.Sigmoid()
-
-        return sigmoid(out)
 
     @staticmethod
     def conv_block(in_channels, out_channels, k=5, p=0.5):
