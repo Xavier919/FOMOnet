7c60bf881f32faac96be12f9565418fbfe88a9ef
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-07-06 08:26:15 -0400

    no m

--- prog/modules/model.py
+++ prog/modules/model.py
@@ -136,14 +136,14 @@ class FOMOnet(nn.Module):
         return block
     
     @staticmethod
-    def res_block(in_channels, out_channels, k=5):
+    def res_block(in_channels, out_channels, k=5, p=0.5):
         block = nn.Sequential(
             #nn.Conv1d(in_channels, in_channels, kernel_size=k, groups=in_channels, padding='same'),
             #nn.Conv1d(in_channels, out_channels, kernel_size=1, padding='same'),
             nn.Conv1d(in_channels, out_channels, kernel_size=k, padding='same'),
             nn.PReLU(),
             nn.BatchNorm1d(out_channels),
-            #nn.Dropout(p=p)
+            nn.Dropout(p=p)
         )
         return block
 
