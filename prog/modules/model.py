6503770fc5f57a8a25eae8a4a3d058b3d5f1401c
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-07-06 20:36:44 -0400

    no m

--- prog/modules/model.py
+++ prog/modules/model.py
@@ -17,8 +17,7 @@ class FOMOnet(nn.Module):
         self.conv4 = self.conv_block(128, 256)
         self.conv5 = self.conv_block(256, 512)
         self.conv6 = self.conv_block(512, 1024)
-        self.conv7 = self.conv_block(1024, 2048)
-        self.convbot = self.conv_block(2048, 2048)
+        self.convbot = self.conv_block(1024, 1024)
         #encoder residual blocks
         self.res1 = self.res_block(num_channels, 32)
         self.res2 = self.res_block(32, 64)
@@ -26,10 +25,8 @@ class FOMOnet(nn.Module):
         self.res4 = self.res_block(128, 256)
         self.res5 = self.res_block(256, 512)
         self.res6 = self.res_block(512, 1024)
-        self.res7 = self.res_block(1024, 2048)
-        self.resbot = self.res_block(2048, 2048)
+        self.resbot = self.res_block(1024, 1024)
         #decoder convolutional blocks
-        self.dconv7 = self.conv_block(2048, 1024)
         self.dconv6 = self.conv_block(1024, 512)
         self.dconv5 = self.conv_block(512, 256)
         self.dconv4 = self.conv_block(256, 128)
@@ -37,14 +34,12 @@ class FOMOnet(nn.Module):
         self.dconv2 = self.conv_block(64, 32)
         self.dconv1 = self.final_block(32, 1)
         #decoder residual blocks
-        self.dres7 = self.res_block(2048, 1024)
         self.dres6 = self.res_block(1024, 512)
         self.dres5 = self.res_block(512, 256)
         self.dres4 = self.res_block(256, 128)
         self.dres3 = self.res_block(128, 64)
         self.dres2 = self.res_block(64, 32)
         #decoder upsampling operations
-        self.upsample7 = nn.ConvTranspose1d(in_channels=2048, out_channels=1024, kernel_size=2, stride=2)
         self.upsample6 = nn.ConvTranspose1d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
         self.upsample5 = nn.ConvTranspose1d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
         self.upsample4 = nn.ConvTranspose1d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
@@ -78,18 +73,10 @@ class FOMOnet(nn.Module):
         #encoder layer 6
         block6 = self.conv6(x) + self.res6(x)
         x = self.maxpool(block6)
-        #encoder layer 7
-        block7 = self.conv7(x) + self.res7(x)
-        x = self.maxpool(block7)
         #bottleneck layer
         bottleneck = self.convbot(x) + self.resbot(x)
-        #decoder layer 7
-        upsamp7 = self.upsample7(bottleneck)
-        cropped7 = self.crop(upsamp7, block7)
-        cat7 = torch.cat((upsamp7, cropped7), 1)
-        x = self.dconv7(cat7) + self.dres7(cat7)
         #decoder layer 6
-        upsamp6 = self.upsample6(x)
+        upsamp6 = self.upsample6(bottleneck)
         cropped6 = self.crop(upsamp6, block6)
         cat6 = torch.cat((upsamp6, cropped6), 1)
         x = self.dconv6(cat6) + self.dres6(cat6)
