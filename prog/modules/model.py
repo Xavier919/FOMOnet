1071daf79b14eaa9b0945afc742fba6e76afd743
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-07-08 18:04:03 -0400

    no m

--- prog/modules/model.py
+++ prog/modules/model.py
@@ -107,7 +107,7 @@ class FOMOnet(nn.Module):
         return self.sigmoid(out)
 
     @staticmethod
-    def conv_block(in_channels, out_channels, k=3, p=0.5):
+    def conv_block(in_channels, out_channels, k=5, p=0.5):
         block = nn.Sequential(
             nn.Conv1d(in_channels, in_channels, kernel_size=k, groups=in_channels, padding='same'),
             nn.Conv1d(in_channels, out_channels, kernel_size=1, padding='same'),
@@ -123,14 +123,13 @@ class FOMOnet(nn.Module):
         return block
     
     @staticmethod
-    def res_block(in_channels, out_channels, k=3, p=0.5):
+    def res_block(in_channels, out_channels, k=5, p=0.5):
         block = nn.Sequential(
-            #nn.Conv1d(in_channels, in_channels, kernel_size=k, groups=in_channels, padding='same'),
-            #nn.Conv1d(in_channels, out_channels, kernel_size=1, padding='same'),
-            nn.Conv1d(in_channels, out_channels, kernel_size=k, padding='same'),
+            nn.Conv1d(in_channels, in_channels, kernel_size=k, groups=in_channels, padding='same'),
+            nn.Conv1d(in_channels, out_channels, kernel_size=1, padding='same'),
             nn.PReLU(),
             nn.BatchNorm1d(out_channels),
-            #nn.Dropout(p=p)
+            nn.Dropout(p=p)
         )
         return block
 
