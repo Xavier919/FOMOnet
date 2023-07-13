43d68ba27de1f359ae982b222dd0c4f87d44c054
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-07-12 20:38:19 -0400

    no m

--- prog/modules/evaluate.py
+++ prog/modules/evaluate.py
@@ -8,7 +8,7 @@ from sklearn.metrics import recall_score
 from torch.nn.utils.rnn import pad_sequence
 import torch
 #from tqdm.notebook import tqdm
-from modules.utils import *
+from utils import *
 
 def bin_pred(output, thresh):
     bin_pred = (output>thresh).int()
