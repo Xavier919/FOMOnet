31524d4011dcafb12d617e8bc86b65bbf57fefae
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-07-15 16:04:50 -0400

    no m

--- prog/modules/eval_script.py
+++ prog/modules/eval_script.py
@@ -19,6 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('data')
 parser.add_argument('model')
 parser.add_argument('kernel', type=int)
+parser.add_argument('dropout', type=float)
 parser.add_argument('trxps', type=str)
 parser.add_argument('tag', type=str)
 args = parser.parse_args()
@@ -103,7 +104,7 @@ if __name__ == "__main__":
     trxps = pickle.load(open(args.trxps, 'rb'))
     _, trxps, _, _ = train_test_split(trxps, trxps, test_size=0.1, random_state=42)
 
-    fomonet = FOMOnet(k=args.kernel)
+    fomonet = FOMOnet(k=args.kernel, d=args.dropout)
     fomonet.load_state_dict(torch.load(args.model, map_location=torch.device('cuda')))
 
     preds = get_preds(fomonet, X_test)
