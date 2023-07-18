583ffb96a31eb99f69b761cfde3648d380478f7d
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-07-18 12:20:16 -0400

    no m

--- prog/modules/model.py
+++ prog/modules/model.py
@@ -98,12 +98,12 @@ class FOMOnet(nn.Module):
         block = nn.Sequential(
             nn.Conv1d(in_channels, in_channels, kernel_size=k, groups=in_channels, padding='same'),
             nn.Conv1d(in_channels, out_channels, kernel_size=1, padding='same'),
-            #nn.BatchNorm1d(out_channels),
             nn.GELU(),
+            nn.BatchNorm1d(out_channels),
             nn.Conv1d(out_channels, out_channels, kernel_size=k, groups=out_channels, padding='same'),
             nn.Conv1d(out_channels, out_channels, kernel_size=1, padding='same'),
-            #nn.BatchNorm1d(out_channels),
             nn.GELU(),
+            nn.BatchNorm1d(out_channels),
         )
         return block
 
@@ -111,7 +111,7 @@ class FOMOnet(nn.Module):
     def final_block(in_channels, out_channels, k=1):
         block = nn.Sequential(
             nn.Conv1d(in_channels, out_channels, kernel_size=k, padding='same'),
-            #nn.BatchNorm1d(out_channels),
             nn.GELU(),
+            nn.BatchNorm1d(out_channels),
         )
         return block
