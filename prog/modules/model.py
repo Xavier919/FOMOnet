0d8558d25a08f03121b270d0687ffe13f489bcdc
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-07-31 05:01:25 -0400

    no m

--- prog/modules/model.py
+++ prog/modules/model.py
@@ -18,22 +18,20 @@ class FOMOnet(nn.Module):
         self.conv4 = self.conv_block(128, 256, k=k)
         self.conv5 = self.conv_block(256, 512, k=k)
         self.conv6 = self.conv_block(512, 1024, k=k)
-        self.convbot = self.conv_block(1024, 1024, k=k)
+        self.conv7 = self.conv_block(1024, 1024, k=k)
         #decoder convolutional blocks
         self.dconv6 = self.conv_block(1024, 512, k=k)
         self.dconv5 = self.conv_block(512, 256, k=k)
         self.dconv4 = self.conv_block(256, 128, k=k)
         self.dconv3 = self.conv_block(128, 64, k=k)
         self.dconv2 = self.conv_block(64, 32, k=k)
-        self.dconv1 = self.conv_block(32, 16, k=k)
-        self.dconv0 = self.final_block(16, 1, k=k)
+        self.dconv1 = self.final_block(32, 1, k=k)
         #decoder upsampling operations
         self.upsample6 = nn.ConvTranspose1d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
         self.upsample5 = nn.ConvTranspose1d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
         self.upsample4 = nn.ConvTranspose1d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
         self.upsample3 = nn.ConvTranspose1d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
         self.upsample2 = nn.ConvTranspose1d(in_channels=64, out_channels=32, kernel_size=2, stride=2)
-        self.upsample1 = nn.ConvTranspose1d(in_channels=32, out_channels=16, kernel_size=2, stride=2)
         #output function
         self.sigmoid = nn.Sigmoid()
 
@@ -45,97 +43,76 @@ class FOMOnet(nn.Module):
     def forward(self, x):
         init_shape = x.shape[2]
         #encoder layer 1
-        block1 = self.conv1(x) 
+        block1 = self.conv1(x)
         x = self.maxpool(block1)
-        x = self.dropout(x)
         
         #encoder layer 2
         block2 = self.conv2(x) 
         x = self.maxpool(block2)
-        x = self.dropout(x)
 
         #encoder layer 3
         block3 = self.conv3(x) 
         x = self.maxpool(block3)
-        x = self.dropout(x)
 
         #encoder layer 4
         block4 = self.conv4(x) 
         x = self.maxpool(block4)
-        x = self.dropout(x)
 
         #encoder layer 5
         block5 = self.conv5(x) 
         x = self.maxpool(block5)
-        x = self.dropout(x)
 
         #encoder layer 6
         block6 = self.conv6(x) 
-        x = self.maxpool(block6)
-        x = self.dropout(x)
 
         #bottleneck layer
-        x = self.convbot(x)
-        x = self.dropout(x)
+        x = self.conv7(block6)
 
         #decoder layer 6
         upsamp6 = self.upsample6(x)
         cropped6 = self.crop(upsamp6, block6)
         cat6 = torch.cat((upsamp6, cropped6), 1)
         x = self.dconv6(cat6)
-        x = self.dropout(x)
 
         #decoder layer 5
         upsamp5 = self.upsample5(x)
         cropped5 = self.crop(upsamp5, block5)
         cat5 = torch.cat((upsamp5, cropped5), 1)
         x = self.dconv5(cat5)
-        x = self.dropout(x)
 
         #decoder layer 4
         upsamp4 = self.upsample4(x)
         cropped4 = self.crop(upsamp4, block4)
         cat4 = torch.cat((upsamp4, cropped4), 1)
         x = self.dconv4(cat4)
-        x = self.dropout(x)
         
         #decoder layer 3
         upsamp3 = self.upsample3(x)
         cropped3 = self.crop(upsamp3, block3)
         cat3 = torch.cat((upsamp3, cropped3), 1)
         x = self.dconv3(cat3)
-        x = self.dropout(x)
 
         #decoder layer 2
         upsamp2 = self.upsample2(x)
         cropped2 = self.crop(upsamp2, block2)
         cat2 = torch.cat((upsamp2, cropped2), 1)
         x = self.dconv2(cat2)
-        x = self.dropout(x)
-
-        #decoder layer 1
-        upsamp1 = self.upsample1(x)
-        cropped1 = self.crop(upsamp1, block1)
-        cat1 = torch.cat((upsamp1, cropped1), 1)
-        x = self.dconv1(cat1)
-        x = self.dropout(x)
 
         #decoder layer 1 (final layer)
-        x = self.dconv0(x)
-        x = self.dropout(x)
-        out = F.interpolate(x, init_shape)
-        return self.sigmoid(out)
+        x = self.dconv1(x)
+        x = F.interpolate(x, init_shape)
+        return self.sigmoid(x)
 
     @staticmethod
     def conv_block(in_channels, out_channels, k=5):
         block = nn.Sequential(
             nn.Conv1d(in_channels, in_channels, kernel_size=k, groups=in_channels, padding='same'),
             nn.Conv1d(in_channels, out_channels, kernel_size=1, padding='same'),
-            nn.PReLU(),
+            nn.GELU(),
             nn.BatchNorm1d(out_channels),
             nn.Conv1d(out_channels, out_channels, kernel_size=k, groups=out_channels, padding='same'),
             nn.Conv1d(out_channels, out_channels, kernel_size=1, padding='same'),
-            nn.PReLU(),
+            nn.GELU(),
             nn.BatchNorm1d(out_channels),
         )
         return block
@@ -144,7 +121,7 @@ class FOMOnet(nn.Module):
     def final_block(in_channels, out_channels, k=1):
         block = nn.Sequential(
             nn.Conv1d(in_channels, out_channels, kernel_size=k, padding='same'),
-            nn.PReLU(),
+            nn.GELU(),
             nn.BatchNorm1d(out_channels),
         )
         return block
