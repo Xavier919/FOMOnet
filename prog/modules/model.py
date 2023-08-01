883b1948f5053fd2dd7aa1fa7c0273962a09c5c1
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-08-01 10:34:23 -0400

    no m

--- prog/modules/model.py
+++ prog/modules/model.py
@@ -126,6 +126,10 @@ class FOMOnet(nn.Module):
             nn.Conv1d(out_channels, out_channels, kernel_size=1, padding='same'),
             nn.GELU(),
             nn.BatchNorm1d(out_channels),
+            nn.Conv1d(out_channels, out_channels, kernel_size=k, groups=out_channels, padding='same'),
+            nn.Conv1d(out_channels, out_channels, kernel_size=1, padding='same'),
+            nn.GELU(),
+            nn.BatchNorm1d(out_channels),
         )
         return block
 
