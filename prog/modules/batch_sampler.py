9098c74ed1e622536973eba573e341baa39d1a99
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-10-01 12:35:50 -0400

    no m

--- prog/modules/batch_sampler.py
+++ prog/modules/batch_sampler.py
@@ -11,16 +11,31 @@ class BatchSampler(torch.utils.data.Sampler):
         self.indices = list(range(len(data_source)))
         self.indices.sort(key=lambda x: len(data_source[x][0]))
         self.class_bins = np.array_split(self.indices, self.num_classes)
+        self.available_bins = list(self.class_bins)
 
     def _batch_indices(self):
-        # Flatten the list of bins and shuffle
-        all_indices = [idx for bin in self.class_bins for idx in bin]
-        np.random.shuffle(all_indices)
+        # While there are enough indices to form a batch
+        while True:
+            batch = []
+            while len(batch) < self.batch_size:
+                # If all bins are exhausted, reset available bins
+                if not self.available_bins:
+                    self.available_bins = list(self.class_bins)
 
-        # Yield batches from the shuffled indices
-        for i in range(0, len(all_indices), self.batch_size):
-            batch = all_indices[i:i+self.batch_size]
-            if len(batch) == self.batch_size:  # Only yield complete batches
+                # Randomly select a bin from available bins
+                chosen_bin_idx = np.random.choice(len(self.available_bins))
+                chosen_bin = self.available_bins[chosen_bin_idx]
+                
+                # If the chosen bin has enough indices, sample without replacement
+                if len(chosen_bin) >= self.batch_size - len(batch):
+                    batch.extend(np.random.choice(chosen_bin, self.batch_size - len(batch), replace=False))
+                # If not, sample all from the chosen bin and remove it from available bins
+                else:
+                    batch.extend(chosen_bin)
+                    del self.available_bins[chosen_bin_idx]
+
+            # If we've collected enough indices for a batch, yield it
+            if len(batch) == self.batch_size:
                 yield batch
 
     def __iter__(self):
@@ -28,3 +43,8 @@ class BatchSampler(torch.utils.data.Sampler):
 
     def __len__(self):
         return len(self.data_source) // self.batch_size
+
+
+
+
+
