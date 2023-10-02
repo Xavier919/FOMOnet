251499f9160ca93a0c1d7f4228309e086a580a3c
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-10-02 16:33:18 -0400

    no m

--- prog/modules/eval_script.py
+++ prog/modules/eval_script.py
@@ -49,27 +49,6 @@ def get_preds(model, X_test):
         preds.append(out)
     return preds
 
-
-#def get_mask_iou(model, X_test, y_test):
-#    iou_lists = []
-#    model.eval()
-#    w_size = 5
-#    for X,y in zip(X_test,y_test):
-#        iou_list = []
-#        pad = torch.zeros(4,X.shape[-1]//2)
-#        for i in range(0,X.shape[-1]):
-#            X_ = X.clone().T
-#            X_[i:i+w_size] = torch.tensor([0.,0.,0.,0.])
-#            X_ = X_.T
-#            X_ = torch.cat([pad,X_,pad],dim=1).view(1,4,-1).cuda()
-#            out = model(X_).view(-1)
-#            out = out[pad.shape[1]:-pad.shape[1]].cpu().detach()
-#            pred = bin_pred(out, 0.5)
-#            iou = iou_score(y, pred)
-#            iou_list.append(iou)
-#        iou_lists.append(iou_list)
-#    return iou_lists
-
 def get_xFOMO(model, X_test, y_test):
     model.eval()
     list_xscores = []
@@ -118,23 +97,34 @@ if __name__ == "__main__":
     
     X_test = [map_seq(x) for x in seqs_test]
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    #fomonet = FOMOnet(k=args.kernel).to(device)
+    #if torch.cuda.device_count() > 1:
+    #    print("Using", torch.cuda.device_count(), "GPUs")
+    #    fomonet = nn.DataParallel(fomonet)
+    #checkpoint = torch.load(args.model)
+    #state_dict = {key.replace("module.", ""): value for key, value in checkpoint.items()}
+    #fomonet.load_state_dict(state_dict)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     fomonet = FOMOnet(k=args.kernel).to(device)
+    # Load the checkpoint first
+    checkpoint = torch.load(args.model, map_location=device)
+    state_dict = {key.replace("module.", ""): value for key, value in checkpoint['state_dict'].items()}
+    # If you have multiple GPUs, then use DataParallel
     if torch.cuda.device_count() > 1:
         print("Using", torch.cuda.device_count(), "GPUs")
         fomonet = nn.DataParallel(fomonet)
-    checkpoint = torch.load(args.model)
-    state_dict = {key.replace("module.", ""): value for key, value in checkpoint.items()}
+
     fomonet.load_state_dict(state_dict)
 
+    preds = get_preds(fomonet, X_test)
 
-    #preds = get_preds(fomonet, X_test)
+    orfs = get_orfs(preds, seqs_test, trxps)
 
-    #orfs = get_orfs(preds, seqs_test, trxps)
-
-    #pickle.dump((preds, y_test), open(f'preds_{args.tag}.pkl', 'wb'))
-    #pickle.dump(orfs, open(f'orfs_{args.tag}.pkl', 'wb'))
+    pickle.dump((preds, y_test), open(f'preds_{args.tag}.pkl', 'wb'))
+    pickle.dump(orfs, open(f'orfs_{args.tag}.pkl', 'wb'))
 
     xFOMO = get_xFOMO(fomonet, X_test, y_test)
 
