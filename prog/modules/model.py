0dc27af4e7561013d273de7bea6f25ee97082929
Xavier919 <xavier.lapointe@usherbrooke.ca>
2023-12-22 15:59:03 -0500

    no m

--- prog/modules/model.py
+++ prog/modules/model.py
@@ -41,7 +41,7 @@ class FOMOnet(nn.Module):
         return enc_ftrs
 
     def forward(self, x):
-        init_shape = x.shape[-1]
+        init_shape = x.shape[2]
         #encoder layer 1
         block1 = self.conv1(x)
         x = self.maxpool(block1)
@@ -82,7 +82,7 @@ class FOMOnet(nn.Module):
 
         #decoder layer 5
         upsamp5 = self.upsample5(x)
-        upsamp5 = F.interpolate(x, e5_shape[-1])
+        upsamp5 = F.interpolate(upsamp5, e5_shape[2])
         cropped5 = self.crop(upsamp5, block5)
         cat5 = torch.cat((upsamp5, cropped5), 1)
         x = self.dconv5(cat5)
@@ -90,7 +90,7 @@ class FOMOnet(nn.Module):
 
         #decoder layer 4
         upsamp4 = self.upsample4(x)
-        upsamp4 = F.interpolate(x, e4_shape[-1])
+        upsamp4 = F.interpolate(upsamp4, e4_shape[2])
         cropped4 = self.crop(upsamp4, block4)
         cat4 = torch.cat((upsamp4, cropped4), 1)
         x = self.dconv4(cat4)
@@ -98,7 +98,7 @@ class FOMOnet(nn.Module):
         
         #decoder layer 3
         upsamp3 = self.upsample3(x)
-        upsamp3 = F.interpolate(x, e3_shape[-1])
+        upsamp3 = F.interpolate(upsamp3, e3_shape[2])
         cropped3 = self.crop(upsamp3, block3)
         cat3 = torch.cat((upsamp3, cropped3), 1)
         x = self.dconv3(cat3)
@@ -106,7 +106,7 @@ class FOMOnet(nn.Module):
 
         #decoder layer 2
         upsamp2 = self.upsample2(x)
-        upsamp2 = F.interpolate(x, e2_shape[-1])
+        upsamp2 = F.interpolate(upsamp2, e2_shape[2])
         cropped2 = self.crop(upsamp2, block2)
         cat2 = torch.cat((upsamp2, cropped2), 1)
         x = self.dconv2(cat2)
@@ -114,7 +114,7 @@ class FOMOnet(nn.Module):
 
         #decoder layer 1
         upsamp1 = self.upsample1(x)
-        upsamp1 = F.interpolate(x, e1_shape[-1])
+        upsamp1 = F.interpolate(upsamp1, e1_shape[2])
         cropped1 = self.crop(upsamp1, block1)
         cat1 = torch.cat((upsamp1, cropped1), 1)
         x = self.dconv1(cat1)
