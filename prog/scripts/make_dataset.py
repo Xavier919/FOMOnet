56fd78f171df0ed8f898d0005a54402e5e5e23fc
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-01-11 21:06:12 -0500

    no m

--- prog/scripts/make_dataset.py
+++ prog/scripts/make_dataset.py
@@ -8,5 +8,16 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
         ensembl_trx = pickle.load(open(args.data, 'rb'))
-        alt_dataset = Data.alt_dataset(ensembl_trx)
-        Data.split_dataset(alt_dataset, 'alt_split')
+
+        dataset = dict()
+        for trx, attrs in ensembl_trx.items():
+            seq, seq_len, chr = attrs['sequence'], len(attrs['sequence']), attrs['chromosome']
+            seq_tensor = torch.zeros(seq_len)
+            dataset[trx] = {'mapped_seq': seq,
+                            'mapped_cds': seq_tensor.view(1,-1),
+                            'chromosome': chr}
+            
+        pickle.dump(dataset, open('dataset_.pkl', 'wb'))
+
+        #alt_dataset = Data.alt_dataset(ensembl_trx)
+        #Data.split_dataset(alt_dataset, 'alt_split')
