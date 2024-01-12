6a4044025c90b6cec0db9b80db3d97e39a7c456f
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-01-11 21:00:17 -0500

    no m

--- prog/scripts/make_dataset.py
+++ prog/scripts/make_dataset.py
@@ -3,10 +3,10 @@ from ..modules.build_dataset import *
 import pickle
 
 parser = argparse.ArgumentParser()
-parser.add_argument('ensembl_trx')
+parser.add_argument('data')
 args = parser.parse_args()
 
 if __name__ == "__main__":
-        ensembl_trx = pickle.load(open(args.ensembl_trx, 'rb'))
+        ensembl_trx = pickle.load(open(args.data, 'rb'))
         alt_dataset = Data.alt_dataset(ensembl_trx)
         Data.split_dataset(alt_dataset, 'alt_split')
