2e8b839c255849cca778ef0a5de92141aa09327c
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-01-11 20:45:46 -0500

    no m

--- prog/scripts/make_dataset.py
+++ prog/scripts/make_dataset.py
@@ -8,5 +8,5 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
         ensembl_trx = pickle.load(open(args.ensembl_trx, 'rb'))
-        alt_dataset = data.alt_dataset(ensembl_trx)
-        data.split_dataset(alt_dataset, 'alt_split')
+        alt_dataset = alt_dataset(ensembl_trx)
+        split_dataset(alt_dataset, 'alt_split')
